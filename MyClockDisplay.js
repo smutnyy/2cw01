13eb99f8d241030a30c7f4d4c8a1a3cbe15cc07f
xWestu <118978444+xWestu@users.noreply.github.com>
2024-01-23 21:04:47 +0100

    Add files via upload

---
+++ MyClockDisplay.js
@@ -0,0 +1,22 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    
+    if(h == 0){
+        h = 12;
+
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
